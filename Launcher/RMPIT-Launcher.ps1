84643e9f12ae1332439916517a98cb1bf4e80fc0
Rickie M. Proctor II <rickie@rickieproctor.com>
2026-03-15 18:53:37 -0400

    1.1.1

--- Launcher/RMPIT-Launcher.ps1
+++ Launcher/RMPIT-Launcher.ps1
@@ -1,4 +1,13 @@
 
+
+function Get-HotReload {
+     
+     $Form.Close()
+     $headers = @{Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YjczMWMxMzMxNmQwYzQ2NTJkM2I5YyIsImlhdCI6MTc3MzYxNTIwNiwiZXhwIjoxNzczNjE1NTA2fQ.tMWafdf38cQMRPhdLmTmmHqvEp6mdQJ7N_vhyGot0WI"}
+     $response = Invoke-RestMethod -Uri "https://app.poshgui.com/api/hotreload/winform/69b731c13316d0c4652d3b9c" -Method Get -Headers $headers
+     iex $response
+    }
+
 <# 
 .NAME
     This is at test
@@ -20,12 +29,20 @@ $logo.height                     = 30
 $logo.location                   = New-Object System.Drawing.Point(42,518)
 $logo.imageLocation              = "https://raw.githubusercontent.com/rickpro2/RMPIT-TechToolkit/main/logo.png"
 $logo.SizeMode                   = [System.Windows.Forms.PictureBoxSizeMode]::zoom
-$RMPITTechToolkit.controls.AddRange(@($logo))
+$hotreload                       = New-Object system.Windows.Forms.PictureBox
+$hotreload.width                 = 60
+$hotreload.height                = 30
+$hotreload.location              = New-Object System.Drawing.Point(915,1)
+$hotreload.imageLocation         = "https://app.poshgui.com/images/refresh.png"
+$hotreload.SizeMode              = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+$RMPITTechToolkit.controls.AddRange(@($logo,$hotreload))
 
 
 
 
 
+
+$hotreload.Add_Click({Get-HotReload})
 
 
 [void]$RMPITTechToolkit.ShowDialog()
