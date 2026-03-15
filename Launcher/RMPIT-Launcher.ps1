b19c8a128bd3307ad97ef769eb7bdcbe6957ab4f
Rickie M. Proctor II <rickie@rickieproctor.com>
2026-03-15 18:46:25 -0400

    1.1.1

--- Launcher/RMPIT-Launcher.ps1
+++ Launcher/RMPIT-Launcher.ps1
@@ -12,12 +12,13 @@ $RMPITTechToolkit                = New-Object system.Windows.Forms.Form
 $RMPITTechToolkit.ClientSize     = New-Object System.Drawing.Point(975,600)
 $RMPITTechToolkit.text           = "Windows 11 Debloat & System Helper By RMPIT LLC v.1.1"
 $RMPITTechToolkit.TopMost        = $false
+$RMPITTechToolkit.icon           = "https://raw.githubusercontent.com/rickpro2/RMPIT-TechToolkit/main/favicon.ico"
 
 $logo                            = New-Object system.Windows.Forms.PictureBox
 $logo.width                      = 60
 $logo.height                     = 30
 $logo.location                   = New-Object System.Drawing.Point(42,518)
-$logo.imageLocation              = "https://github.com/rickpro2/RMPIT-TechToolkit/logo.png"
+$logo.imageLocation              = "https://raw.githubusercontent.com/rickpro2/RMPIT-TechToolkit/main/logo.png"
 $logo.SizeMode                   = [System.Windows.Forms.PictureBoxSizeMode]::zoom
 $RMPITTechToolkit.controls.AddRange(@($logo))
 
