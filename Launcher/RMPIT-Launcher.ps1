514cc761ab46ef4e15fdc84a718a1848d362bb46
Rickie M. Proctor II <rickie@rickieproctor.com>
2026-03-15 18:38:01 -0400

    1.1.1

--- Launcher/RMPIT-Launcher.ps1
+++ Launcher/RMPIT-Launcher.ps1
@@ -1,13 +1,4 @@
 
-
-function Get-HotReload {
-     
-     $Form.Close()
-     $headers = @{Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YjczMWMxMzMxNmQwYzQ2NTJkM2I5YyIsImlhdCI6MTc3MzYxMzU5NCwiZXhwIjoxNzczNjEzODk0fQ.uUZdJhSeMdVzbYD0gYGtBhHxdiuRjNL0QhLRuBRRAro"}
-     $response = Invoke-RestMethod -Uri "https://app.poshgui.com/api/hotreload/winform/69b731c13316d0c4652d3b9c" -Method Get -Headers $headers
-     iex $response
-    }
-
 <# 
 .NAME
     This is at test
@@ -18,24 +9,22 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $RMPITTechToolkit                = New-Object system.Windows.Forms.Form
-$RMPITTechToolkit.ClientSize     = New-Object System.Drawing.Point(400,400)
-$RMPITTechToolkit.text           = "Form"
+$RMPITTechToolkit.ClientSize     = New-Object System.Drawing.Point(975,600)
+$RMPITTechToolkit.text           = "Windows 11 Debloat & System Helper By RMPIT LLC v.1.1"
 $RMPITTechToolkit.TopMost        = $false
 
-$hotreload                       = New-Object system.Windows.Forms.PictureBox
-$hotreload.width                 = 60
-$hotreload.height                = 30
-$hotreload.location              = New-Object System.Drawing.Point(340,1)
-$hotreload.imageLocation         = "https://app.poshgui.com/images/refresh.png"
-$hotreload.SizeMode              = [System.Windows.Forms.PictureBoxSizeMode]::zoom
-$RMPITTechToolkit.controls.AddRange(@($hotreload))
+$logo                            = New-Object system.Windows.Forms.PictureBox
+$logo.width                      = 60
+$logo.height                     = 30
+$logo.location                   = New-Object System.Drawing.Point(42,518)
+$logo.imageLocation              = "https://github.com/rickpro2/RMPIT-TechToolkit/logo.png"
+$logo.SizeMode                   = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+$RMPITTechToolkit.controls.AddRange(@($logo))
 
 
 
 
 
-
-$hotreload.Add_Click({Get-HotReload})
 
 
 [void]$RMPITTechToolkit.ShowDialog()
